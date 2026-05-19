19e49533172f4bdc701c3e95b1b8356c6fe15eb7
Memo <fretn655@gmail.com>
2026-05-18 18:37:33 -0600

    se crean las 3 librerias que usaremos para el proyecto, trabajadas durante el capitulo completo.

---
+++ Proyecto8_DS_LuisT.py
@@ -0,0 +1,8 @@
+# ------------- Importacion de librerias
+
+import geopandas as gpd
+import pandas as pd
+from shapely.geometry import Point
+
+# ------------- Finalizacion de las Importacion de librerias
+
